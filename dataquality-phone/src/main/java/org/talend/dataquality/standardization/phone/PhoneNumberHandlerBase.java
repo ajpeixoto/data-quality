8991ae7d5cca84855574a072a70fb9d1203f8e0b
sbliu <sbliu@talend.com>
2021-03-04 18:25:57 +0800

    feat(TUP-30114) Update Copyrights to 2021 for master code (#909)

--- dataquality-phone/src/main/java/org/talend/dataquality/standardization/phone/PhoneNumberHandlerBase.java
+++ dataquality-phone/src/main/java/org/talend/dataquality/standardization/phone/PhoneNumberHandlerBase.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2016 qiongli Inc. - www.qiongli.com
+// Copyright (C) 2006-2021 qiongli Inc. - www.qiongli.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.qiongli.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
