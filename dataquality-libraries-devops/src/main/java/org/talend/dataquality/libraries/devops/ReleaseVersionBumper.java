ea8e643f9afcea7d943f7f402db340f7cfdbdc5a
build-talend-tdq <59256349+build-talend-tdq@users.noreply.github.com>
2021-10-21 09:49:42 +0200

    chore(Jenkins): Bump to next dev version 9.1.0-SNAPSHOT (#980)
    Co-authored-by: jenkins-git <jenkins-git@talend.com>

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -66,7 +66,7 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "9.0.3-SNAPSHOT";
+    private static final String TARGET_VERSION = "9.1.0-SNAPSHOT";
 
     private static final String TARGET_DAIKON_VERSION = "5.11.0";
 
