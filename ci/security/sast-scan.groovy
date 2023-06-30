79b839291fed147a44848a51fb0f07daea35bbb0
Liu Xinquan <xqliu@talend.com>
2023-06-30 19:54:47 +0800

    chore(TDQ-21266) enable sca for 7.3 (#1141)

---
+++ ci/security/sast-scan.groovy
@@ -0,0 +1,107 @@
+#!/usr/bin/groovy
+def AWS_POD_LABEL = "TDQ-SE-${UUID.randomUUID().toString()}"
+
+def veracodeCredentials = usernamePassword(
+        credentialsId: 'veracode-api-credentials',
+        passwordVariable: 'VERACODE_KEY',
+        usernameVariable: 'VERACODE_ID')
+
+pipeline {
+    agent {
+        kubernetes {
+            label AWS_POD_LABEL
+            yamlFile 'ci/podTemplate.yml'
+        }
+    }
+    
+    triggers {
+         cron(BRANCH_NAME == 'maintenance/8.0' ? '0 15 * * 0' : '')
+    }
+
+    options {
+        // Only keep the 10 most recent builds for master branch, 5 for the other branches
+        buildDiscarder(logRotator(artifactNumToKeepStr: '5', numToKeepStr: env.BRANCH_NAME == 'master' ? '10' : '5'))
+        disableConcurrentBuilds()
+        ansiColor('xterm')
+        timeout(time: 1, unit: 'HOURS')
+    }
+
+    environment {
+        SLACK_CHANNEL = 'tdq_ci'
+        VERACODE_APP_NAME = 'data-quality'
+        TIMESTAMP = sh(returnStdout: true, script: "date +%Y%m%d_%H%M%S").trim()
+    }
+
+    stages {
+
+        stage('Prepare artifacts') {
+            steps {
+                container('talend-jdk8-builder-base') {
+                    configFileProvider([configFile(fileId: 'maven-settings-nexus-zl', variable: 'MAVEN_SETTINGS')]) {
+                        sh 'java -version'
+                        sh 'mvn --version'
+                        sh 'mvn -U clean package -DskipTests -B --fail-at-end -s $MAVEN_SETTINGS'
+                    }
+                }
+            }
+        }
+
+        stage('Veracode-Upload-and-Scan') {
+            steps {
+                container('talend-jdk8-builder-base') {
+                    withCredentials([veracodeCredentials]) {
+                        veracode applicationName: "$VERACODE_APP_NAME",
+                            canFailJob: true,
+                            createProfile: false,
+                            debug: true,
+                            copyRemoteFiles: true,
+                            fileNamePattern: '',
+                            useProxy: false,
+                            replacementPattern: '',
+                            scanExcludesPattern: '',
+                            scanIncludesPattern: '',
+                            scanName: "DQ-${env.BUILD_NUMBER}-${TIMESTAMP}",
+                            uploadExcludesPattern: '',
+                            uploadIncludesPattern: '**/**.jar',
+                            vid: "$VERACODE_ID",
+                            vkey: "$VERACODE_KEY"
+                    }
+                }
+            }
+        }
+
+    }
+
+    post {
+        success {
+            script {
+                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME =~ 'maintenance.*') {
+                    slackSend(color: '#82bd41', channel: "${SLACK_CHANNEL}", message: "SUCCESS: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)")
+                }
+            }
+        }
+        failure {
+            script {
+                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME =~ 'maintenance.*') {
+                    slackSend(color: '#e96065', channel: "${SLACK_CHANNEL}", message: "FAILED: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)")
+                }
+            }
+        }
+
+        unstable {
+            script {
+                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME =~ 'maintenance.*') {
+                    slackSend(color: '#ea8330', channel: "${SLACK_CHANNEL}", message: "UNSTABLE: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)")
+                }
+            }
+        }
+
+        aborted {
+            script {
+                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME =~ 'maintenance.*') {
+                    slackSend(color: '#c6c6c6', channel: "${SLACK_CHANNEL}", message: "ABORTED: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)")
+                }
+            }
+        }
+    }
+}
---
+++ ci/security/sca-scan.groovy
@@ -0,0 +1,89 @@
+#!/usr/bin/groovy
+
+def AWS_POD_LABEL = "TDQ-VERACODE-${UUID.randomUUID().toString()}"
+def scanResultPath = 'scan-result.json'
+def slackChannel = 'tdq_ci'
+
+
+pipeline {
+    agent {
+        kubernetes {
+            label AWS_POD_LABEL
+            yamlFile 'ci/podTemplate.yml'
+        }
+    }
+
+    triggers {
+        cron(BRANCH_NAME == "maintenance/8.0" ? "0 15 * * 0" : "")
+    }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timeout(time: 90, unit: 'MINUTES')
+        disableConcurrentBuilds()
+    }
+
+    stages {
+        stage('Build project') {
+            steps {
+                container('talend-jdk8-builder-base') {
+                    configFileProvider([configFile(fileId: 'maven-settings-nexus-zl', variable: 'MAVEN_SETTINGS')]) {
+                        sh 'java -version'
+                        sh 'mvn --version'
+                        sh 'mvn -U clean package -DskipTests -B --fail-at-end -s $MAVEN_SETTINGS'
+                    }
+                }
+            }
+        }
+
+        stage('Scan 3rd parties vulnerabilities') {
+            steps {
+                container('talend-jdk8-builder-base') {
+                    withCredentials([string(credentialsId: 'veracode-token', variable: 'SRCCLR_API_TOKEN')]) {
+                        sh """#!/bin/bash
+
+                    # In case of trouble use DEBUG=1 sh -s -- scan...
+                      
+                    curl -sSL https://download.sourceclear.com/ci.sh | sh -s -- scan --json  > ${scanResultPath}
+                """
+                    }
+                }
+            }
+        }
+
+        stage('Process scan result') {
+            steps {
+                echo 'Build simple report & send slack notification'
+                container('talend-jdk8-builder-base') {
+                    script {
+                        readFile "${scanResultPath}"
+                        SCAN_RESULT_URL = sh(
+                                script: "jq --raw-output '.records[0].metadata.report' ${scanResultPath}",
+                                returnStdout: true
+                        ).trim()
+                        echo "Scan result URL: ${SCAN_RESULT_URL}"
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            script {
+                slackSend (
+                        color: '#82bd41',
+                        channel: slackChannel,
+                        message: "SUCCESSFUL: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>) <${SCAN_RESULT_URL}|Full scan result>")
+            }
+        }
+        failure {
+            script {
+                slackSend (
+                        color: '#e96065',
+                        channel: slackChannel,
+                        message: "FAILED: `${env.JOB_NAME.replaceAll('%2F', '/')}` #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>) <${SCAN_RESULT_URL}|Full scan result>")
+            }
+        }
+    }
+}
